61513a643ff4a622af61d5b74783ba84661119f7
Luiz Assunção <luiz.bazon@hotmail.com>
2018-08-09 16:48:51 -0400

    create process to award referrer

--- referral-api/app/models/referral.rb
+++ referral-api/app/models/referral.rb
@@ -2,4 +2,15 @@ class Referral < ApplicationRecord
   belongs_to :contact
 
   validates_presence_of :name, :email
+
+  after_create :award_referrer
+
+  private
+
+  DEFAULT_POINTS_AWARD = 100
+
+  def award_referrer
+    new_score = contact.points + DEFAULT_POINTS_AWARD
+    Contact.update(contact.id, points: new_score)
+  end
 end
--- referral-api/spec/models/referral_spec.rb
+++ referral-api/spec/models/referral_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Referral, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
   end
+
+  context 'when creating a referral' do
+    it 'awards referrer' do
+      contact = FactoryBot.create(:contact)
+      FactoryBot.create(:referral, contact_id: contact.id)
+      FactoryBot.create(:referral, contact_id: contact.id)
+      result = Contact.first
+
+      expect(result.points).to eq 200
+    end
+  end
 end
