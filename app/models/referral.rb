ca123211ca93e7dece87258d699c3a3374e0b9f9
Luiz Assunção <luiz.bazon@hotmail.com>
2018-08-10 02:13:01 -0400

    refactor observers notification in referral model

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -18,19 +18,20 @@ class Referral < ApplicationRecord
     new_score = contact.points + DEFAULT_POINTS_AWARDED
     Contact.update(contact.id, points: new_score)
 
-    payload = {
-      event_type: Enums::EventType::REFERRAL_AWARDED,
-      points: DEFAULT_POINTS_AWARDED,
-      descriptions: build_event_message(contact_id: contact_id, new_score: new_score)
-    }
-    notify_observers(payload)
+    notify_observers(build_event_message(contact_id: contact.id, new_score: new_score))
   end
 
   def build_event_message(contact_id:, new_score:)
     "Referral awarded to contact id: #{contact_id}. New score: #{new_score}"
   end
 
-  def notify_observers(payload)
-    self.notifiers.each { |notifier| notifier.new.notify(payload: payload) }
+  def notify_observers(message)
+    payload = {
+      event_type: Enums::EventType::REFERRAL_AWARDED,
+      points: DEFAULT_POINTS_AWARDED,
+      descriptions: message
+    }
+
+    notifiers.each { |notifier| notifier.new.notify(payload: payload) }
   end
 end
